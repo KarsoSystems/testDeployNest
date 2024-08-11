cad4f6bddf7c999eb75935d8cb4d344bfa9f9c5d
Carlos Rodriguez <ce.rodriguez240391@gmail.com>
2024-08-11 13:07:34 -0600

    feat: test nest deploy en lambda

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap();
+//https://soshace.com/2024/01/30/nest-js-and-aws-lambda-for-serverless-microservices/
+//AKIAXYKJVQ37EISWICBK
+//BwNM/vUdVRPNutmU1zuUM6nnQooJzMz6hy9j4TgF
