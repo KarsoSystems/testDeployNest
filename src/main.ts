abffd7f078d3a44b78bc7e2aee2523aa4b3b6e6e
Carlos Rodriguez <ce.rodriguez240391@gmail.com>
2024-08-20 22:55:28 -0600

    feat: ajuste de puerto

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
 //https://soshace.com/2024/01/30/nest-js-and-aws-lambda-for-serverless-microservices/
